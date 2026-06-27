6ef08efd9a58f5c2f6aef31134202078414253fe
vhidalgo0 <vhidalgo2025@alu.uct.cl>
2026-06-27 18:47:20 -0400

    Corrige modulo de usuarios

--- gtm/src/usuarios/usuarios.module.ts
+++ gtm/src/usuarios/usuarios.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from './usuario.entity';
 import { UsuariosController } from './usuarios.controller';
 import { UsuariosService } from './usuarios.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
+  imports: [JwtModule.register({}), TypeOrmModule.forFeature([Usuario])],
   controllers: [UsuariosController],
   providers: [UsuariosService],
   exports: [UsuariosService],

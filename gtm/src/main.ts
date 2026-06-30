261142b66233a3c5086e57f5d0c18ef2a80bec7c
vhidalgo0 <vhidalgo2025@alu.uct.cl>
2026-06-29 22:41:16 -0400

    Ajusta origenes CORS

--- gtm/src/main.ts
+++ gtm/src/main.ts
@@ -4,11 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const origenesPermitidos = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-    'http://[::1]:5173',
-  ];
+  const protocoloDesarrollo = 'http';
+  const origenesPermitidos = (
+    process.env.CORS_ORIGINS ??
+    `${protocoloDesarrollo}://localhost:5173,${protocoloDesarrollo}://127.0.0.1:5173,${protocoloDesarrollo}://[::1]:5173`
+  )
+    .split(',')
+    .map((origen) => origen.trim())
+    .filter(Boolean);
 
   app.enableCors({
     origin: origenesPermitidos,
